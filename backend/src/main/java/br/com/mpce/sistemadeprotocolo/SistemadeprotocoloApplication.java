a2dfccb0e60867fa032713cb4f830d29a9ed838b
ValdenirRomanJunior <vromanjunior@outlook.com>
2021-10-17 21:39:04 -0300

    primeiro commit

---
+++ backend/src/main/java/br/com/mpce/sistemadeprotocolo/SistemadeprotocoloApplication.java
@@ -0,0 +1,13 @@
+package br.com.mpce.sistemadeprotocolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadeprotocoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadeprotocoloApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/mpce/sistemadeprotocolo/SistemadeprotocoloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mpce.sistemadeprotocolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadeprotocoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
